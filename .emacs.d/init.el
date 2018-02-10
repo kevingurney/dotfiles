b7194d4c3c2d3dfd545d071f788e0dc4c7b08a1c
Kevin Gurney <kevin.p.gurney@gmail.com>
2018-02-10 15:54:41 -0500

    Modify .emacs.d/init.el to automatically install use-package for Emacs Lisp package management

---
+++ .emacs.d/init.el
@@ -0,0 +1,22 @@
+;; Automatic installation of use-package inspired by:
+;; https://github.com/CachesToCaches/getting_started_with_use_package
+
+;; Require package.el.
+(require 'package)
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; Configure Emacs Lisp package archives.
+
+;; Enable the GNU Emacs Lisp Package Archive (ELPA).
+(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/"))
+;; Enable Milkypostman's Emacs Lisp Package Archive (MELPA).
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+;; Automatically install use-package for Emacs Lisp package management.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
